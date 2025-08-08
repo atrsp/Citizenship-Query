68d062ff09f85eecc31ef8f96ee119fd1d67a292
atrsp <atrspereira@gmail.com>
2025-08-08 20:01:20 -0300

    Lógica corrigida e comentários adicionados

--- main.pl
+++ main.pl
@@ -1,11 +1,21 @@
-descendente(X, Y) :- (mae(Y, X); pai(Y, X)); ((pai(Y, Z); mae(Y, Z)), descendente(X, Z)). #bug
+# Avalia se uma pessoa é descendente de outra.
+descendente(X, Y) :- (pai(Y, X); mae(Y, X)); (pai(Z, X); mae(Z, X)), descendente(Z, Y).
+
+# Avalia se uma pessoa é avô ou avó de outra.
 avo(X, Y) :- (pai(Z, Y); mae(Z, Y)), (mae(X, Z); pai(X, Z)).
 
+# Avalia se uma pessoa tem direito à cidadania alemã.
+    # REGRA: Transmissão de cidadania até netos de alemães.
 tem_direito(X, alemanha) :- (avo(Z, X); pai(Z, X); mae(Z, X)), cidadania(Z, alemanha).
-tem_direito(X, portugal) :- (avo(Z, X); pai(Z, X); mae(Z, X)), cidadania(Z, portugal).
-tem_direito(X, reino_unido) :- (pai(Z,X); mae(Z, X)), cidadania(Z, reino_unido).
-tem_direito(X, italia) :- descendente(X, Y), cidadania(Y, italiana). #bug
 
-#?- tem_direito(joao, italia).
-#?- tem_direito(joao, Y).
-#?- tem_direito(X, Y).
+# Avalia se uma pessoa tem direito à cidadania portuguesa.
+    # REGRA: Transmissão de cidadania até netos de portugueses.
+tem_direito(X, portugal) :- (avo(Z, X); pai(Z, X); mae(Z, X)), cidadania(Z, portugal).
+
+# Avalia se uma pessoa tem direito à cidadania britânica.
+    # REGRA: A cidadania deve ser transmitida por um pai ou mãe que é cidadão britânico.
+tem_direito(X, reino_unido) :- (pai(Z,X); mae(Z, X)), cidadania(Z, reino_unido).
+
+# Avalia se uma pessoa tem direito à cidadania italiana.
+    # REGRA: O direito à cidadania é transmitido sem limite de gerações.
+tem_direito(X, italia) :- descendente(X, Y), cidadania(Y, italia).
